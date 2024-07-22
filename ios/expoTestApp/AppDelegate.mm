c49c999062a2c7e09e776d56d5a3256188efb65b
akshay-engage <114734802+akshay-engage@users.noreply.github.com>
2024-07-22 19:52:31 +0530

    Working iOS for prebuild

--- ios/expoTestApp/AppDelegate.mm
+++ ios/expoTestApp/AppDelegate.mm
@@ -2,6 +2,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
+#import <WebEngage/WebEngage.h>
 
 @implementation AppDelegate
 
@@ -12,6 +13,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  [[WebEngage sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
